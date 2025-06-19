8f2958dfc8859229d30719d9c4b41d70c509ea31
INDIAjuliano <juliano.gasii1@gmail.com>
2025-06-19 19:57:56 +0300

    correction de route

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ResgisterComponent } from './pages/resgister/resgister.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'dashobard', pathMatch: 'full' },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: ResgisterComponent }
+
+        ]
+    }
+];
